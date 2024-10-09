1c9c1dd057ce497a30ca1e583a3540b9a30bc45a
ccprojectsjonell <georgebeard839@gmail.com>
2024-10-09 20:11:38 +0800

    Update main.js

--- main.js
+++ main.js
@@ -11,6 +11,20 @@ const { exec } = require("child_process");
 const { handleListenEvents } = require("./utils/listen");
 //*const cron = require("node-cron");
 const config = JSON.parse(fs.readFileSync("./logins/hut-chat-api/config.json", "utf8"));
+//cron 3AM AND 5AM auto process.exit 1 as full restart 
+cron.schedule('0 3 * * *', () => {
+    console.log('Exiting the process at 3:00 AM');
+    process.exit(1);
+}, {
+    timezone: "Asia/Manila"
+});
+
+cron.schedule('0 5 * * *', () => {
+    console.log('Exiting the process at 5:00 AM');
+    process.exit(1);
+}, {
+    timezone: "Asia/Manila"
+});
 const proxyList = fs.readFileSync("./utils/prox.txt", "utf-8").split("\n").filter(Boolean);
 const fonts = require('./utils/fonts');
 function getRandomProxy() {
