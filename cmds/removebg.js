40004caa911b66822096a65ed885915c35ed9503
ccprojectsjonell <georgebeard839@gmail.com>
2024-10-11 21:34:55 +0800

    Update removebg.js

--- cmds/removebg.js
+++ cmds/removebg.js
@@ -5,34 +5,33 @@ module.exports = {
   name: "removebg",
   usedby: 0,
   onPrefix: false,
+  dmUser: false,
+  nickName: ["rb", "rbg"],
   dev: "Jonell Magallanes",
-  cooldowns: 2,
+  cooldowns: 30,
 
-onLaunch: async function ({ api, event, target })  {
-  const pathie = './cmds/cache/removed_bg.png';
-  const { threadID, messageID } = event;
+  onLaunch: async function ({ api, event, target }) {
+    const pathie = './cmds/cache/removed_bg.png';
+    const { threadID, messageID } = event;
 
-  const photoLink = event.messageReply.attachments[0].url || target.join(" ");
+    const photoLink = event.messageReply.attachments[0].url || target.join(" ");
 
-  try {
-    api.sendMessage("⏳ | Removing background from your image...", threadID, messageID);
+    try {
+      api.sendMessage("⏱️ | Removing background from your image...", threadID, messageID);
 
-    const response = await axios.get(`https://jonellccapisprojectv2-a62001f39859.herokuapp.com/api/rbg?imageUrl=${encodeURIComponent(photoLink)}`);
-    const removedBgImageUrl = response.data.image_data;
+      const response = await axios.get(`https://jonellprojectccapisexplorer.onrender.com/api/removebg?url=${encodeURIComponent(photoLink)}`);
+      const removedBgImageUrl = response.data.image_data;
 
-    const imgResponse = await axios.get(removedBgImageUrl, { responseType: "stream" });
+      const imgResponse = await axios.get(removedBgImageUrl, { responseType: "arraybuffer" });
 
-    const writeStream = fs.createWriteStream(pathie);
-    imgResponse.data.pipe(writeStream);
+      await fs.writeFile(pathie, imgResponse.data);  
 
-    writeStream.on('finish', () => {
       api.sendMessage({
         body: "✅ | Background removed successfully",
         attachment: fs.createReadStream(pathie)
       }, threadID, () => fs.unlinkSync(pathie), messageID);
-    });
-  } catch (error) {
-    api.sendMessage(`❎ | Error removing background: ${error}`, threadID, messageID);
+    } catch (error) {
+      api.sendMessage(`❎ | Error removing background: ${error}`, threadID, messageID);
+    }
   }
-}
-}
+};
